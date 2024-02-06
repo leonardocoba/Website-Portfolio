3ad22b5eb2f0f09594801eaaaf988d4e081a6c66
Leonardo D. Cobaleda <leonardocobaleda097@gmail.com>
2024-02-06 00:21:48 -0500

    Removed Loader

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,15 +1,9 @@
-// Examples
-// https://www.benrogers.dev/#projects
-// https://availchet.github.io/contact
-// https://github.com/HackathonHackers/personal-sites
-
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 import Navbar from "./components/NavBar";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
-import Loader from "./components/loader";
 import Contact from "./components/contact";
 import Projects from "./components/Projects";
 import "./global.css";
@@ -22,16 +16,6 @@ function App() {
   const educationRef = useRef(null);
   const contactRef = useRef(null);
 
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowLoader(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div
       className="App"
@@ -39,40 +23,34 @@ function App() {
         backgroundColor: "#FFFAFA",
       }}
     >
-      {showLoader ? (
-        <Loader />
-      ) : (
-        <>
-          <Navbar
-            sectionRefs={{
-              home: homeRef,
-              skills: skillsRef,
-              experience: experienceRef,
-              projects: projectsRef,
-              education: educationRef,
-              contact: contactRef,
-            }}
-          />
-          <div className="home" ref={homeRef}>
-            <Home />
-          </div>
-          <div className="skills" ref={skillsRef}>
-            <Skills />
-          </div>
-          <div className="experience" ref={experienceRef}>
-            <Experience />
-          </div>
-          <div className="projects" ref={projectsRef}>
-            <Projects />
-          </div>
-          <div className="education" ref={educationRef}>
-            <Education />
-          </div>
-          <div className="contact" ref={contactRef}>
-            <Contact />
-          </div>
-        </>
-      )}
+      <Navbar
+        sectionRefs={{
+          home: homeRef,
+          skills: skillsRef,
+          experience: experienceRef,
+          projects: projectsRef,
+          education: educationRef,
+          contact: contactRef,
+        }}
+      />
+      <div className="home" ref={homeRef}>
+        <Home />
+      </div>
+      <div className="skills" ref={skillsRef}>
+        <Skills />
+      </div>
+      <div className="experience" ref={experienceRef}>
+        <Experience />
+      </div>
+      <div className="projects" ref={projectsRef}>
+        <Projects />
+      </div>
+      <div className="education" ref={educationRef}>
+        <Education />
+      </div>
+      <div className="contact" ref={contactRef}>
+        <Contact />
+      </div>
     </div>
   );
 }
