146f4364e428d92758c5054aa812a55900e31f25
Leonardo D. Cobaleda <leonardocobaleda097@gmail.com>
2024-03-03 03:12:43 -0500

    Fixed Education formatting

--- my-portfolio/src/components/Education.js
+++ my-portfolio/src/components/Education.js
@@ -10,15 +10,13 @@ function Education({ isNightMode }) {
         flexDirection: "column",
         alignItems: "center",
         textAlign: "center",
-        paddingTop: "15vh",
+        paddingTop: "5vh", // Space above the title
+        paddingBottom: "5vh", // Space below the content
       }}
     >
-      <h1>My Education</h1>
+      <h1 style={{ marginBottom: "2vh" }}>My Education</h1>
       <div
         style={{
-          height: "300px",
-          width: "100%",
-          maxWidth: "600px",
           display: "flex",
           backgroundColor: isNightMode
             ? darkModeBackgroundColor
@@ -26,41 +24,53 @@ function Education({ isNightMode }) {
           borderRadius: "10px",
           boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
           overflow: "hidden",
-          justifyContent: "center",
+          width: "100%",
+          maxWidth: "750px",
+          margin: "0 auto", // Centers the card in the container
         }}
       >
         <div
           style={{
-            width: "100%",
-            height: "100%",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
+            padding: "20px",
           }}
         >
           <img
             src={`${process.env.PUBLIC_URL}/imgs/uf.png`}
             alt="University Logo"
-            style={{ maxWidth: "100%", maxHeight: "200%" }}
+            style={{ maxHeight: "100%", maxWidth: "250px" }} // Adjusted image size
           />
         </div>
-
-        {/* Content container */}
-        <div style={{ width: "70%", padding: "20px", textAlign: "left" }}>
-          <h3 style={{ margin: 0 }}>University of Florida</h3>
-          <p>Bachelor of Science in Computer Science</p>
-          <p>GPA: 3.4</p>
-          <p>• Minor in Entrepreneurship</p>
-          <p>
-            • Major Course work: Data Structures and Algorithms, Information and
-            Databases, Data Science for ECE, Intro to SWE, Artificial
-            Intelligence Fundamentals
+        <div
+          style={{
+            width: "70%",
+            padding: "20px", // Uniform padding for top and bottom
+            textAlign: "left",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center", // This centers the content vertically
+          }}
+        >
+          <h3 style={{ margin: "0", lineHeight: "1.5" }}>
+            University of Florida
+          </h3>
+          <p style={{ lineHeight: "1.5" }}>
+            Bachelor of Science in Computer Science
           </p>
-          <p>
-            • Artifical Intelligence fundamentals and Applications Certification
-            (IP){" "}
-          </p>
-          {/* Add more details as needed */}
+          <p style={{ lineHeight: "1.5" }}>GPA: 3.4</p>
+          <ul style={{ paddingLeft: "20px", lineHeight: "1.5" }}>
+            <li>Minor in Entrepreneurship</li>
+            <li>
+              Major Coursework: Data Structures and Algorithms, Information and
+              Databases, Data Science for ECE, Intro to SWE
+            </li>
+            <li>
+              Artificial Intelligence Fundamentals and Applications
+              Certification (IP)
+            </li>
+          </ul>
         </div>
       </div>
     </div>
