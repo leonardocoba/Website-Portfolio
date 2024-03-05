eaabd8e60602005692fa30c0d3f5a73636465a44
Leonardo D. Cobaleda <116767849+leonardocoba@users.noreply.github.com>
2024-03-05 15:40:03 -0500

    updated  home formting

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -2,90 +2,33 @@ import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import DownloadIcon from "@mui/icons-material/Download";
-import "./Home.css";
+import "./Home.css"; // Make sure the CSS file is correctly imported
 
-import ParticleBackground from "./particles";
+// Import the ParticleBackground component if used
+// import ParticleBackground from "./particles";
 
 function Home() {
   return (
-    <div
-      className="home"
-      style={{
-        height: "75vh",
-        width: "100%",
-        position: "relative",
-        display: "flex",
-        backgroundColor: "#1A202C",
-        border: "1px solid #000",
-      }}
-    >
-      {/* <ParticleBackground /> */}
+    <div className="home">
+      {/* <ParticleBackground /> Optional Particle Background */}
 
-      <div
-        style={{
-          width: "25%",
-          height: "100%",
-          position: "relative",
-          zIndex: 1,
-        }}
-      >
-        <img
-          src={`${process.env.PUBLIC_URL}/imgs/PFP.png`}
-          alt="Profile"
-          style={{
-            height: "100%",
-            width: "auto",
-            position: "absolute",
-            bottom: "0",
-            left: "-70px",
-          }}
-        />
+      <div className="profile-image-container">
+        <img src={`${process.env.PUBLIC_URL}/imgs/PFP.png`} alt="Profile" />
       </div>
 
-      <div
-        style={{
-          width: "50%",
-          display: "flex",
-          flexDirection: "column",
-          zIndex: 1,
-        }}
-      >
-        <div
-          style={{
-            flex: 1,
-            padding: "20px",
-            boxSizing: "border-box",
-            display: "flex", // Use flexbox to center content
-            flexDirection: "column", // Stack children vertically
-            justifyContent: "center", // Center vertically in the container
-            alignItems: "center", // Center horizontally in the container
-            height: "100%", // Ensure the div takes full height of the parent
-            textAlign: "center", // Center text alignment
-            marginTop: "10rem",
-          }}
-        >
-          <h1 className="welcome"> Welcome to my Website!</h1>
-          <p className="intro">
-            I'm Leonardo, but you can call me Leo. I'm an aspiring software
-            engineer studying at the University of Florida. This summer I'm
-            excited to enhance my skills through an internship at Microsoft in
-            Atlanta. I invite you to explore my website and get a full picture
-            of my professional and academic journey!
-          </p>
-        </div>
+      <div className="main-content">
+        <h1 className="welcome">Welcome to my Website!</h1>
+        <p className="intro">
+          I'm Leonardo, but you can call me Leo. I'm an aspiring software
+          engineer studying at the University of Florida. This summer I'm
+          excited to enhance my skills through an internship at Microsoft in
+          Atlanta. I invite you to explore my website and get a full picture of
+          my professional and academic journey!
+        </p>
 
-        <div
-          style={{
-            flex: 1,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "20px",
-          }}
-        >
+        <div className="buttons-section">
           <a
             href="https://github.com/leonardocoba"
-            style={{ textDecoration: "none" }}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -96,7 +39,6 @@ function Home() {
           </a>
           <a
             href="https://www.linkedin.com/in/leonardo-cobaleda/"
-            style={{ textDecoration: "none" }}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -106,11 +48,10 @@ function Home() {
             </button>
           </a>
           <a
-            href={`${process.env.PUBLIC_URL}/extras/Leonardo%20Cobaleda%20Resume.pdf`} // Encoded URL for your PDF
-            download="Leonardo_Cobaleda_Resume.pdf" // Suggested filename for download
-            style={{ textDecoration: "none" }}
-            target="_blank" // Optional: Opens the PDF in a new tab
-            rel="noopener noreferrer" // Security measure for target='_blank'
+            href={`${process.env.PUBLIC_URL}/extras/Leonardo%20Cobaleda%20Resume.pdf`}
+            download="Leonardo_Cobaleda_Resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <button className="Btn">
               Download Resume
